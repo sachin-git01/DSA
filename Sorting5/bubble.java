ed34d043e2ac1a4639dfae73ec3309b750d112db
Sachin Kumar <sachinofficial7310@gmail.com>
2026-01-29 00:32:48 +0530

    Implement bubble sort algorithm in bubble.java

---
+++ Sorting5/bubble.java
@@ -0,0 +1,8 @@
+for(int i=0;i<arr.length;i++){
+  int swap=0;
+  for(int j=0;j<arr.length-1-i;j++){
+    if(arr[j]>arr[j+1]){
+      swap(arr[j],arr[j+1]){
+      }
+      if(swap==0) break;
+      
