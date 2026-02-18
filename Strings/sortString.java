54d824e82a86e43a58ccdd5812b54280464329c7
Sachin Kumar <sachinofficial7310@gmail.com>
2026-02-18 18:08:03 +0530

    Create sortString.java to handle string sorting
    Initialize sortString.java with main method to process input strings.

---
+++ Strings/sortString.java
@@ -0,0 +1,20 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+
+        // input string
+        StringBuilder sb =new StringBuilder();
+          for(String a: args){
+              
+              sb.append(a);
+          }
+          char []a=sb.toString().toCharArray();
+          
+    }
+}
+
