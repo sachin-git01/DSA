47d43a0ab6953fb8a3bb4a52ba65b2c3620b817e
Sachin Kumar <sachinofficial7310@gmail.com>
2026-02-22 11:49:41 +0530

    Add nonRepeatingChar method to find unique char
    Implement method to find the first non-repeating character in a string.

---
+++ Strings/first_non_rep.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char nonRepeatingChar(String s) {
+        // code here
+        int freq[]=new int[256];
+        for(char c : s.toCharArray()){
+            
+            freq[c]++;
+            
+        }
+             for (char c : s.toCharArray()) {
+            if (freq[c] == 1) {
+                return c;
+            }
+        }
+
+        return '$';
+    }
+}
