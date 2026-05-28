81dbf0c9d8b724b28aa3911a155f78418c1dab33
Sachin Kumar <sachinofficial7310@gmail.com>
2026-05-28 15:25:43 +0530

    Add removeDuplicates method to Solution class
    Implement removeDuplicates method to remove duplicate nodes from a linked list.

---
+++ LinkedList/remDup.java
@@ -0,0 +1,27 @@
+// head =null for del all ll 
+/* Structure of linked list Node
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+      data = d;
+      next = null;
+    }
+}
+*/
+class Solution {
+    Node removeDuplicates(Node head) {
+        // code here
+        Node temp=head;
+        while(temp!=null && temp.next!=null){
+            if(temp.data==temp.next.data){
+                temp.next=temp.next.next;
+            }
+            else{
+                temp=temp.next;
+            }
+        }
+        return head;
+    }
+}
