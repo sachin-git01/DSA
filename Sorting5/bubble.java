2a7a51292017438936a9c0c3f307cad5e8f7a2b4
Sachin Kumar <sachinofficial7310@gmail.com>
2026-08-08 15:43:48 +0530

    Update bubble.java

--- Sorting5/bubble.java
+++ Sorting5/bubble.java
@@ -1,8 +1,14 @@
-for(int i=0;i<arr.length;i++){
-  int swap=0;
-  for(int j=0;j<arr.length-1-i;j++){
-    if(arr[j]>arr[j+1]){
-      swap(arr[j],arr[j+1]){
-      }
-      if(swap==0) break;
-      
+class Solution {
+    public void bubbleSort(int[] arr) {
+        // code here
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+            }
+        }
+    }
+}
