c47b381794e5faf77ba9fbc77a864ece0dfaad3c
Sachin Kumar <sachinofficial7310@gmail.com>
2026-08-08 17:40:20 +0530

    Add insertion sort implementation
    Implemented the insertion sort algorithm to sort an array in-place.

--- Sorting5/insersion.java
+++ Sorting5/insersion.java
@@ -1,1 +1,17 @@
+// bro code lecture
 
+class Solution {
+    // Please change the array in-place
+    public void insertionSort(int arr[]) {
+        // code here
+          for(int i=0;i<arr.length;i++){
+              int temp=arr[i];
+              int j=i-1;
+              while(j>=0 && arr[j]>temp){
+                  arr[j+1]=arr[j];
+                  j--;
+              }
+              arr[j+1]=temp;
+          }
+    }
+}
