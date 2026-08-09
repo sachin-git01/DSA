be870c392f56466d9d992f1102a5ed3df4f6e86d
Sachin Kumar <sachinofficial7310@gmail.com>
2026-08-09 10:40:37 +0530

    Update InsertAtEnd.java

--- LinkedList/InsertAtEnd.java
+++ LinkedList/InsertAtEnd.java
@@ -1,10 +1,26 @@
-class Solution {
-    public Node insertAtFront(Node head, int x) {
-        // code here
-        Node temp=new Node(x);
-        temp.next=head;
-        head=temp;
-         return head;
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
     }
-   
+}
+*/
+class Solution {
+    public Node insertAtEnd(Node head, int x) {
+        // code here
+        Node newNode=new Node(x);
+  if(head==null){
+      return newNode;
+  }
+  Node temp=head;
+  while(temp.next!=null){
+      temp=temp.next;
+  }
+  temp.next=newNode;
+  return head;
+    }
 }
