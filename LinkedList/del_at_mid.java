f68a284db25f0fd893a443c0f3384dc201175fa1
Sachin Kumar <sachinofficial7310@gmail.com>
2026-08-09 13:10:38 +0530

    Create del_at_mid.java

---
+++ LinkedList/del_at_mid.java
@@ -0,0 +1,25 @@
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    Node deleteMid(Node head) {
+        // This is method only submission.
+        // You only need to complete the method.
+        if(head==null || head.next==null){
+            return null;
+        }
+        Node slow=head;
+        Node fast=head;
+        while(fast.next.next!=null && fast.next.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }
+}
