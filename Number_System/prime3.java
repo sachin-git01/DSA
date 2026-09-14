177fbdae480494adde463383f1fe7729645c3fb4
Sachin Kumar <sachinofficial7310@gmail.com>
2026-09-14 12:06:53 +0530

    Add isPrime method to check for prime numbers

---
+++ Number_System/prime3.java
@@ -0,0 +1,10 @@
+class Solution {
+    static boolean isPrime(int n) {
+        // code here
+        if(n<=1) return false;
+      for(int i=2;i<n;i++){   //  for (int i = 2; i <= Math.sqrt(n); i++) {
+          if(n%i==0) return false;
+       }
+       return true; 
+    }
+}
